6fda027547fcdd60555941cc22a0914f534d8904
timatm <a131677975@gmail.com>
2024-09-30 20:31:26 +0800

    first commit

---
+++ src/shell.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include "../include/command.h"
+#include "../include/builtin.h"
+
+int execute(struct pipes *p)
+{
+	return execvp(p->args[0], p->args);
+}
+
+int spawn_proc(int in, int out, struct cmd *cmd, struct pipes *p)
+{
+  	pid_t pid;
+  	int status, fd;
+  	if ((pid = fork()) == 0) {
+      	if (in != 0) {
+          	dup2(in, 0);
+          	close(in);
+        } else {
+			if (cmd->in_file) {
+				fd = open(cmd->in_file, O_RDONLY);
+				dup2(fd, 0);
+				close(fd);
+			}
+		}
+	    if (out != 1) {
+          	dup2(out, 1);
+          	close(out);
+        } else {
+			if (cmd->out_file) {
+				fd = open(cmd->out_file, O_RDWR | O_CREAT, 0644);
+				dup2(fd, 1);
+				close(fd);
+			}
+		}
+    	if (execute(p) == -1)
+       		perror("lsh");
+    	exit(EXIT_FAILURE);
+    } 
+	else {
+		waitpid(pid, &status, WUNTRACED);
+		while (!WIFEXITED(status) && !WIFSIGNALED(status));
+  	}
+  	return 1;
+}
+
+int fork_pipes(struct cmd *cmd)
+{
+  	int in = 0, fd[2];
+	struct pipes *temp = cmd->head;
+  	spawn_proc(0, 1, cmd, cmd->head);
+	return 1;
+}
+
+void shell()
+{
+	while (1) {
+		printf(">>> $ ");
+		
+		char *buffer = read_line();
+		printf("buffer:%s \n",buffer);
+		if (buffer == NULL)
+			continue;
+
+		struct cmd *cmd = split_line(buffer);
+		
+		int status = -1;
+		//no pipe command
+		builtInCommand(cmd);
+		// if (!cmd->background && cmd->head->next == NULL) {
+		// 	int fd, in = dup(0), out = dup(1);
+		// 	if (cmd->in_file) {
+        //         fd = open(cmd->in_file, O_RDONLY);
+        //         dup2(fd, 0);
+        //         close(fd);
+        //     }
+		// 	if (cmd->out_file) {
+        //         fd = open(cmd->out_file, O_RDWR | O_CREAT, 0644);
+        //         dup2(fd, 1);
+        //         close(fd);
+		// 	}
+		// 	builtInCommand(cmd);
+		// 	if (cmd->in_file)  dup2(in, 0);
+		// 	if (cmd->out_file) dup2(out, 1);
+		// 	close(in);
+		// 	close(out);
+		// }
+		if (status == -1)
+			status = fork_pipes(cmd);
+
+		while (cmd->head) {
+			struct pipes *temp = cmd->head;
+      		cmd->head = cmd->head->next;
+			free(temp->args);
+   	    	free(temp);
+   		}
+		free(cmd);
+		free(buffer);
+		
+		if (status == 0)
+			break;
+	}
+}
